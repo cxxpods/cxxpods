7726dc7bd3c87234a7310b84674c042c606db087
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-07-09 06:57:06 -0700

    - set max listeners on EventEmitter prototype, no remove unwarranted warnings

--- bin/cunit.js
+++ bin/cunit.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 require("source-map-support").install()
-require("babel-polyfill");
+require("babel-polyfill")
 
-const Path = require("path")
+const
+  Events = require("events"),
+  Path = require("path")
 
+// noinspection JSUndefinedPropertyAssignment
+Events.defaultMaxListeners = 100
 require(Path.resolve(__dirname,"..","dist","main.js"))
