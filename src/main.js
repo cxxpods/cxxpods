1ff47425e3580963a7dc3714c2d096c33ff29b30
Jonathan Glanz <jonathan.glanz@gmail.com>
2019-05-21 11:52:30 -0400

    - fixed execution from non-project folder

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+
+
 const
   lockfile = require("lockfile"),
   sh = require("shelljs"),
@@ -16,16 +18,21 @@ function findRootPath() {
   let cxxRootPath = null
   let currentDir = pwd
   while (!_.isEmpty(currentDir)) {
+    if (typeof currentDir !== "string" || currentDir === "")
+      break
+    
     const testPath = `${currentDir}/cxxpods.yml`
     if (sh.test('-e', testPath)) {
       cxxRootPath = currentDir
       break
     }
     
+    
+    
     currentDir = Path.dirname(currentDir)
   }
   
-  return cxxRootPath ? Promise.resolve(cxxRootPath) : Promise.reject(`Unable to find cxxpods.yml in ancestors from: ${pwd}`)
+  return Promise.resolve(cxxRootPath) // : Promise.reject(`Unable to find cxxpods.yml in ancestors from: ${pwd}`)
 }
 
 
@@ -33,26 +40,9 @@ function findRootPath() {
 findRootPath()
   .then(async (cxxRootPath) => {
     
-    const lockfilePath = Path.resolve(Os.platform().toLowerCase().startsWith("win") ? "c:/temp" : "/tmp", `${Buffer.from(cxxRootPath).toString('base64')}.lock`)
-    sh.exec(`sleep ${Math.floor(Math.random() * 5)}`)
+    const disposers = []
     
-    console.log(`Using lock file: ${lockfilePath}`)
-  
-    lockfile.lock(lockfilePath, {wait: 1000 * 60 * 60}, async (err) => {
-      console.log("Received lock")
-  
-  
-      // LOGGER
-      const
-        GetLogger = require("./Log").default,
-        log = GetLogger(__filename)
-  
-      // CHECK ERROR
-      if (err) {
-        log.error(`Failed to get lock: ${lockfilePath}`, err)
-        return
-      }
-  
+    async function run() {
       // ARGS
       let Yargs = require("yargs")
   
@@ -66,16 +56,7 @@ findRootPath()
         const mod = require(file)
         Yargs = mod.default ? mod.default(Yargs) : mod(Yargs)
       }
-  
-      /**
-       * Unlock the lock file
-       */
-      function unlock() {
-        lockfile.unlock(lockfilePath, err => {
-          err && log.error(`Failed to unlock: ${lockfilePath}`, err)
-        })
-      }
-  
+      
       // REPO
       const Repo = require('./repo/Repo')
   
@@ -85,8 +66,10 @@ findRootPath()
       try {
         [
           "./commands/RepoCommands",
-          "./commands/ProjectCommands",
-          "./commands/InfoCommands"
+            ...(!cxxRootPath ? [] : [
+              "./commands/ProjectCommands",
+              "./commands/InfoCommands"
+            ])
         ].forEach(addCommands)
     
         Yargs.demandCommand(1, "You need at least one command before moving on")
@@ -95,9 +78,48 @@ findRootPath()
         log.error(`Failed`, ex)
         console.error(ex)
       } finally {
-        unlock()
+        disposers.forEach(fn => fn())
       }
-    })
+    }
+    
+    if (cxxRootPath) {
+      const lockfilePath = Path.resolve(Os.platform().toLowerCase().startsWith("win") ? "c:/temp" : "/tmp", `${Buffer.from(cxxRootPath).toString('base64')}.lock`)
+      sh.exec(`sleep ${Math.floor(Math.random() * 5)}`)
+  
+      console.log(`Using lock file: ${lockfilePath}`)
+  
+      lockfile.lock(lockfilePath, {wait: 1000 * 60 * 60}, async (err) => {
+        console.log("Received lock")
+    
+    
+        // LOGGER
+        const
+          GetLogger = require("./Log").default,
+          log = GetLogger(__filename)
+    
+        // CHECK ERROR
+        if (err) {
+          log.error(`Failed to get lock: ${lockfilePath}`, err)
+          return
+        }
+    
+    
+        /**
+         * Unlock the lock file
+         */
+        const unlock = () => {
+          lockfile.unlock(lockfilePath, err => {
+            err && log.error(`Failed to unlock: ${lockfilePath}`, err)
+          })
+        }
+    
+        disposers.push(unlock)
+    
+        await run()
+      })
+    } else {
+      await run()
+    }
   })
   .catch(err => console.error(err))
 
